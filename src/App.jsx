81606daa27c5d1d87cda7789cdda93d8e1cca652
RolInTheShell <rolintheshell@gmail.com>
2022-02-03 13:16:59 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+import bg from './res/bg.png';
+
+const App = () => {
+  return (
+            <div className='wrapped'>
+                <img src={bg} alt="" width="80%" />
+                <p>Comming soon...</p>
+            </div>
+  );
+};
+
+export default App;
